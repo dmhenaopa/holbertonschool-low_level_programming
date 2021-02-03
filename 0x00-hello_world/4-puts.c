f1b6073e28e29b915deacc818af77ae2fcd219aa
Diana Henao <2777@holbertonschool.com>
2021-02-03 23:22:52 +0000

    [FIX] Fix an error in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+/**
+* main - Print a string with puts
+*
+* Description: Hello world task
+* Return: 0 if everything OK
+*/
 int main(void)
 {
-
-/*
-*Print a string with puts
-*
-*Description: Hello world task
-*/
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
