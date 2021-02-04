52866775db67229069b166c2e2092f87e0cc9243
Diana Henao <2777@holbertonschool.com>
2021-02-04 21:45:12 +0000

    [FIX] Fix an error in 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - Variables, if, else, while task
 *
@@ -17,18 +18,18 @@ int main(void)
 
 	if (n == 0)
 	{
-		printf("%d/n is zero", n);
+		printf("%d is zero\n", n);
 	}
 
 	else
 	{
 		if (n > 0)
 		{
-			printf("%d/n is negative", n);
+			printf("%d is positive\n", n);
 		}
 		else
 		{
-			printf("%d/n is negative", n);
+			printf("%d is negative\n", n);
 		}
 	}
 
