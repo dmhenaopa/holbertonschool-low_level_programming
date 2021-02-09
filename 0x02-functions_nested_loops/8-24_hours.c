9fa2053b02e9a4a874cbad7e38fcb3ea14747bc5
Diana Henao <2777@holbertonschool.com>
2021-02-09 23:29:26 +0000

    [FIX] Fix an error in 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,19 +16,22 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(58);
-					_putchar('0' + k);
-					_putchar('0' + l);
-					_putchar('\n');
+					if (((i <= 1 && j <= 9) || (i == 2 && j <= 3)) && (k <= 5 && l <= 9))
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(58);
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n');
 					}
+				}
 			}
 		}
 	}
