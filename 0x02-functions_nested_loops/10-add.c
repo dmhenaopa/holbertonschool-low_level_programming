c2923fdf580ad20c379193bce3576789c1c03944
Diana Henao <2777@holbertonschool.com>
2021-02-10 01:10:52 +0000

    [ADD] 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* add - C - Functions, nested loops task
+* @a: Integer a
+* @b: Integer b
+*
+* Description: Write a function that adds two integers
+* and returns the result.
+* Return: result of add two integers
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
