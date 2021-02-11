65459daf26a8a46190f04451057b8ecb595c7f94
Diana Henao <2777@holbertonschool.com>
2021-02-11 19:30:35 +0000

    [ADD] 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* mul - C - More functions, more nested loops
+* @a: integer value to multiply a
+* @b: integer value to multiply b
+*
+* Description: Write a function that multiplies two integers.
+* Return: Result of multiply a and b
+*/
+int mul(int a, int b)
+{
+	int multiplication;
+
+	multiplication = a * b;
+	return (multiplication);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,7 +1,7 @@
 A short description of each script:
 + 0-isupper.c: Write a function that checks for uppercase character.
 + 1-isdigit.c: Write a function that checks for a digit (0 through 9).
-+
++ 2-mul.c: Write a function that multiplies two integers.
 +
 +
 +
