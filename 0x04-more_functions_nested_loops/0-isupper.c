bb1658afa6b77405b58e9f5e06b337924a8a5a58
Diana Henao <2777@holbertonschool.com>
2021-02-11 17:13:09 +0000

    [ADD] 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _isupper - C - More functions, more nested loops
+* @c: number of the character to solve
+*
+* Description: Write a function that checks for uppercase character
+* Return: 1 if c is uppercase character. 0 otherwise.
+*/
+int _isupper(int c)
+{
+	int answer;
+
+	if (c >= 65 && c <= 90)
+	{
+		answer = 1;
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		answer = 0;
+	}
+
+	return (answer);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,5 +1,5 @@
 A short description of each script:
-+
++ -isupper.c: Write a function that checks for uppercase character.
 +
 +
 +
