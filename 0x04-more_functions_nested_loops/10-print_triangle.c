6b85cacf8ed72416d96abd5ac5d7e9255ef50546
Diana Henao <2777@holbertonschool.com>
2021-02-13 16:16:31 +0000

    [ADD] 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+* main - C - More functions, more nested loops
+* @size: Integer size of the triangle.
+*
+* Description: Write a function that prints a triangle,
+* followed by a new line.
+*/
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int count = size - 1;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j == count)
+				{
+					_putchar('#');
+					count--;
+				}
+
+				else if (j > count)
+				{
+					_putchar('#');
+				}
+
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -9,4 +9,4 @@ A short description of each script:
 + 7-print_diagonal.c: Write a function that draws a diagonal line on the terminal.
 + 8-print_square.c: Write a function that prints a square, followed by a new line.
 + 9-fizz_buzz.c: Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
-+
++ 10-print_triangle.c: Write a function that prints a triangle, followed by a new line.
