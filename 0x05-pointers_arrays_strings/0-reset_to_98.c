8ab13028f172fd78bca4b964048cf853771a6ad6
Diana Henao <2777@holbertonschool.com>
2021-02-15 23:07:37 +0000

    [ADD] 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* reset_to_98 - C - Pointers, arrays and strings
+* @*n: pointer value
+*
+* Description: Write a function that takes a pointer to
+* an int as parameter and updates the value it points to 98.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+A short description of each script:
++ 0-reset_to_98.c: Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
++
