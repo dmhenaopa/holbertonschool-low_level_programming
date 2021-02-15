394c7cdedeadbc6856b316d5fc85841f68968e28
Diana Henao <2777@holbertonschool.com>
2021-02-15 23:44:51 +0000

    [FIX] fix betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 * reset_to_98 - C - Pointers, arrays and strings
-* @*n: pointer value
+* @n: pointer value
 *
 * Description: Write a function that takes a pointer to
 * an int as parameter and updates the value it points to 98.
