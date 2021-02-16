03cd7fe86fcdb23c691235ce668cfd7b9c05b167
Diana Henao <2777@holbertonschool.com>
2021-02-16 16:31:24 +0000

    [FIX] fix error in boundaries in second for

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,9 +16,10 @@ void print_rev(char *s)
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		length++;
+
 	}
 
-	for (j = length; j >= 0;  j--)
+	for (j = length - 1; j >= 0;  j--)
 	{
 		_putchar(*(s + j));
 	}
