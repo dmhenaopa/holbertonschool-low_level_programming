8a7869b8ce9db1883c39faad210dc9e77f070743
Diana Henao <2777@holbertonschool.com>
2021-02-16 02:18:51 +0000

    [ADD] 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* swap_int - C - Pointers, arrays and strings
+* @a: Integer pointer a
+* @b: Integer pointer b
+*
+* Description: Write a function that swaps the values of two integers.
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,3 @@
 A short description of each script:
 + 0-reset_to_98.c: Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
-+
++ 1-swap.c: Write a function that swaps the values of two integers.
