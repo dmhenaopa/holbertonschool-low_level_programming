c332a1d4af1ac86ef5707613f0ee096b720da70a
Diana Henao <2777@holbertonschool.com>
2021-02-19 01:48:44 +0000

    [FIX] error in for condition, add i < n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	while (i <= n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
