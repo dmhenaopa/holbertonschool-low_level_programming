4c6f8f10698f578179f0e45082b33cb3f6938d75
Diana Henao <2777@holbertonschool.com>
2021-02-20 01:47:20 +0000

    [ADD] 6-cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* cap_string - C - More pointers, arrays and strings
+* @s: Input string
+*
+* Description: Write a function that capitalizes all words of a string.
+* Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ",
+* (, ), {, and }
+* Return: String with all words capitalized
+*/
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	char separators[14] = { 32, 9, '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}', '\0'};
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (*(s + i) == separators[j])
+			{
+				if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+				{
+					*(s + i + 1) = *(s + i + 1) - 32;
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,6 +3,7 @@ A short description of each script:
 + 1-strncat.c: Write a function that concatenates two strings. It will use at most n bytes from src; and src does not need to be null-terminated if it contains n or more bytes.
 + 2-strncpy.c: Write a function that copies a string.
 + 3-strcmp.c: Write a function that compares two strings.
-+ 
++ 4-rev_array.c: Write a function that reverses the content of an array of integers.
 + 5-string_toupper.c: Write a function that changes all lowercase letters of a string to uppercase.
++ 6-cap_string.c: Write a function that capitalizes all words of a string.
 +
