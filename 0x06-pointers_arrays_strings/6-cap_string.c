d9d1c79b23d07564b068498d5de8d2e731a6b04a
Diana Henao <2777@holbertonschool.com>
2021-02-20 02:52:49 +0000

    [FIX] error in capitalization: only first letter, no other letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 {
 	int i;
 	int j;
-	char separators[14] = { 32, 9, '\n', ',', ';', '.', '!', '?',
+	char separators[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}', '\0'};
 
 	for (i = 0; *(s + i) != '\0'; i++)
@@ -24,6 +24,19 @@ char *cap_string(char *s)
 				if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
 				{
 					*(s + i + 1) = *(s + i + 1) - 32;
+				}
+
+				else
+				{
+					continue;
+				}
+			}
+
+			else if (*(s + i) != separators[j])
+			{
+				if (*(s + i + 2) >= 65 && *(s + i + 2) <= 90)
+				{
+					*(s + i + 2) = *(s + i + 2) + 32;
 				}
 
 				else
