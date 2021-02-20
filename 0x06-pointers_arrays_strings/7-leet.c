008ed381761fea3a0fa39fd875d2309bd8b2697a
Diana Henao <2777@holbertonschool.com>
2021-02-20 23:52:43 +0000

    [FIX] error in number's array - duplicated numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,11 +11,17 @@ char *leet(char *s)
 	int i;
 	int j;
 	char letter_array[] = {"aAeEoOtTlL"};
-	char number_array[] = {"4433007711"};
+	char number_array[] = {"43071"};
 
 	for (i = 0; *(s + i) != '\0'; i++)
-		for (j = 0; *(letter_array + j) != '\0'; j++)
-			if (*(s + i) == *(letter_array + j))
-				*(s + i) = *(number_array + j);
+	{
+		for (j = 0; letter_array[j] != '\0'; j++)
+		{
+			if (*(s + i) == letter_array[j])
+			{
+				*(s + i) = number_array[j / 2];
+			}
+		}
+	}
 	return (s);
 }
