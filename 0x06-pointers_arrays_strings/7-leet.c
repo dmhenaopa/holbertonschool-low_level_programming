224116e5bed63aa5e04f81adfdf3807b547ed696
Diana Henao <2777@holbertonschool.com>
2021-02-21 22:48:49 +0000

    [FIX] elimination of braces in conditional and loop statement

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* leet- C - More pointers, arrays and strings
+* leet - C - More pointers, arrays and strings
 * @s: Input string
 *
 * Description: Write a function that encodes a string into 1337.
@@ -14,14 +14,8 @@ char *leet(char *s)
 	char number_array[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		for (j = 0; letter_array[j] != '\0'; j++)
-		{
 			if (s[i] == letter_array[j])
-			{
 				s[i] = number_array[j / 2];
-			}
-		}
-	}
 	return (s);
 }
