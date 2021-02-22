1eb14e75adf9b218435192126a683a80ddc129b7
Diana Henao <2777@holbertonschool.com>
2021-02-22 22:24:10 +0000

    [ADD] 2-strchr.c file]

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* _strchr - C - Even more pointers, arrays and strings
+* @s: string in which character is searched
+* @c: character that is searched in the string
+*
+* Description: Write a function that locates a character in a string.
+* Return: A pointer to the first occurrence of the character c in
+* the string s, or NULL if the character is not found.
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			s += i;
+			break;
+		}
+
+		else if (*(s + i) == '\0')
+		{
+			*s = '\0';
+		}
+
+		else
+		{
+			continue;
+		}
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,4 +1,4 @@
 A short description of each script:
 + 0-memset.c: Write a function that fills memory with a constant byte.
 + 1-memcpy.c: Write a function that copies memory area.
-+
++ 2-strchr.c: Write a function that locates a character in a string.
