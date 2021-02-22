808e0588fe8e7e4a8b236bb1d947a2dedb325977
Diana Henao <2777@holbertonschool.com>
2021-02-22 23:09:13 +0000

    [FIX] error in NULL value assignation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,6 +11,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	int NULL = 0;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
@@ -29,7 +30,7 @@ char *_strchr(char *s, char c)
 
 	if (*(s + i) == '\0')
 	{
-		*s = 0;
+		*s = NULL;
 	}
 	return (s);
 }
