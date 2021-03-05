542cc05e94e879010d089b187bb953e7dc774053
Diana Henao <2777@holbertonschool.com>
2021-03-05 04:04:45 +0000

    [FIX] separate functions - size of string and concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
+* arrayCounter - C - malloc, free
+* @string: Input string
+*
+* Description: Function that found the size of string.
+* Return: Size of the string.
+*/
+unsigned int arrayCounter(char *string)
+{
+	unsigned int i, counter;
+
+	counter = 0;
+	if (string == NULL)
+		string = "";
+
+	for (i = 0; *(string + i) != '\0'; i++)
+		counter++;
+
+	return (counter);
+}
+
+/**
 * string_nconcat - C - More malloc, free
 * @s1: String to concatenates
 * @s2: String to concatenates
@@ -13,43 +34,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer_concat;
-	unsigned int i, j, k, s1_size, s2_size;
+	unsigned int i, j, s1_size, s2_size;
 
-	/*If NULL -> treat as an empty string*/
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == NULL)
+	else if (s2 == NULL)
 		s2 = "";
 
-	/*To know the size of the first string s1 and s2*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		s1_size = i;
-
-	for (i = 0; *(s2 + i) != '\0'; i++)
-		s2_size = i;
+	s1_size = arrayCounter(s1);
+	s2_size = arrayCounter(s2);
 
 	/* + 1 for null character at the end of the array*/
 	if (n < s2_size)
 		pointer_concat = malloc((s1_size + n + 1) * sizeof(char));
+
 	else if (n >= s2_size)
-		pointer_concat = malloc((s1_size + s2_size + 1) * sizeof(char));
+		pointer_concat = malloc((s1_size + s2_size) * sizeof(char));
 
 	if (pointer_concat == NULL)
 		return (NULL);
 
 	/*Concatenates s1 and s2*/
-	for (j = 0; *(s1 + j) != '\0'; j++)
-		*(pointer_concat + j) = *(s1 + j);
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(pointer_concat + i) = *(s1 + i);
 
 	if (n < s2_size)
-		for (k = 0; *(s2 + k) != '\0' && k < n; k++, j++)
-			*(pointer_concat + j) = *(s2 + k);
+	{
+		for (j = 0; *(s2 + j) != '\0' && j < n; j++, i++)
+			*(pointer_concat + i) = *(s2 + j);
+
+		*(pointer_concat + i) = '\0';
+	}
 
 	else if (n >= s2_size)
-		for (k = 0; *(s2 + k) != '\0'; k++, j++)
-			*(pointer_concat + j) = *(s2 + k);
+		for (j = 0; *(s2 + j) != '\0'; j++, i++)
+			*(pointer_concat + i) = *(s2 + j);
 
-	*(pointer_concat + j) = '\0';
 	return (pointer_concat);
 }
