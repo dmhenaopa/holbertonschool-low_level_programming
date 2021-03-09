8e263b05f664422bdc1e0562a489e88aa2158209
Diana Henao <2777@holbertonschool.com>
2021-03-09 22:26:50 +0000

    [FIX] error betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,48 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+* new_dog - C - Structures, typedef
+* @name: Pointer to name of new dog
+* @age: Age of new dog
+* @owner: Pointer owner
+*
+* Description: A function that creates a new dog
+* Return: Dog information
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *new_new_dog;
-        int i, j;
-        
-        /*i = strlen(name)*/
-        for (i = 0; *(name + i) != '\0'; i++)
-        {
-        }
-        
-        /*j = strlen(owner)*/
-        for (j = 0; *(owner + j) != '\0'; j++)
-        {
-        }
-        
-        new_new_dog = malloc(sizeof(dog_t));
-        (*new_new_dog).name = malloc((i + 1) * sizeof(char));
-        (*new_new_dog).owner = malloc((j + 1) * sizeof(char));
+	dog_t *new_new_dog;
+	int i, j;
 
-        for (i = 0; *(name + i) != '\0'; i++)
-                (*new_new_dog).name[i] = *(name + i);
-        (*new_new_dog).name[i] = '\0';
+	/*i = strlen(name)*/
+	for (i = 0; *(name + i) != '\0'; i++)
+	{
+	}
 
-        for (j = 0; *(owner + j) != '\0'; j++)
-                (*new_new_dog).owner[j] = *(owner + j);
-        (*new_new_dog).owner[j] = '\0';        
+	/*j = strlen(owner)*/
+	for (j = 0; *(owner + j) != '\0'; j++)
+	{
+	}
 
-        if (new_new_dog == NULL)
-                return (NULL);
+	new_new_dog = malloc(sizeof(dog_t));
+	(*new_new_dog).name = malloc((i + 1) * sizeof(char));
+	(*new_new_dog).owner = malloc((j + 1) * sizeof(char));
 
-        /*(*new_new_dog).name = name;*/
-        (*new_new_dog).age = age;
-        /*(*new_new_dog).owner = owner;*/
-        return (new_new_dog);
+	for (i = 0; *(name + i) != '\0'; i++)
+		(*new_new_dog).name[i] = *(name + i);
+	(*new_new_dog).name[i] = '\0';
+
+	for (j = 0; *(owner + j) != '\0'; j++)
+		(*new_new_dog).owner[j] = *(owner + j);
+	(*new_new_dog).owner[j] = '\0';
+
+	if (new_new_dog == NULL)
+		return (NULL);
+
+	/*(*new_new_dog).name = name;*/
+	(*new_new_dog).age = age;
+	/*(*new_new_dog).owner = owner;*/
+	return (new_new_dog);
 }
