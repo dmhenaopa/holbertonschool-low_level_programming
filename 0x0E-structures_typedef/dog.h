8741da7d2e9b68c0de29667e7122fc5e0edaddcc
Diana Henao <2777@holbertonschool.com>
2021-03-09 02:45:53 +0000

    [ADD] dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog -C - Structures, typedef
+* @name: Name of dog
+* @age: Age of dog
+* @owner: Owner of dog
+*
+* Description: A new type struct dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
