a0eb13ac4f77df6d61155233b1c3757e642f84da
Diana Henao <2777@holbertonschool.com>
2021-03-10 22:27:04 +0000

    [FIX] error when the argument pointer is NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(*(array + i));
+	/*Handling possible errors*/
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(*(array + i));
+	}
 }
