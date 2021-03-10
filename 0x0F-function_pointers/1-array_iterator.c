e0ada2f3cbd26777170316fa00fd5844dcba604e
Diana Henao <2777@holbertonschool.com>
2021-03-10 21:44:53 +0000

    [ADD] 1-array_iterator.c file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+* array_iterator - C - Function pointers
+* @array: elements
+* @size: size of array
+* @action: pointer to function
+*
+* Description: A unction that executes a function
+* given as a parameter on each element of an array.
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(*(array + i));
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,2 +1,4 @@
 A short description of each script:
 + 0-print_name.c: A function that prints a name.
++ 1-array_iterator.c: A function that executes a function given as a parameter on each element of an array.
++
