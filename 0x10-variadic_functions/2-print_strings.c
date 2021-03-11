394c7a06fd359ad5cb4ca8ddbb0e6c0ecfd2f5cf
Diana Henao <2777@holbertonschool.com>
2021-03-11 22:17:49 +0000

    [ADD] 2-print_strings.c file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+* print_strings - C - Variadic functions
+* @separator: string to be printed between the strings.
+* @n: number of strings passed to the function.
+*
+* Description: A function that prints strings,
+* followed by a new line.
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string_list;
+	unsigned int i;
+	char *toprint;
+
+	/*Initalize list*/
+	va_start(string_list, n);
+
+	/*Get the values*/
+	for (i = 0; i < n; i++)
+	{
+		toprint = va_arg(string_list, char*);
+		printf("%s", toprint);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(string_list);
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,4 +1,5 @@
 A short description of each script:A short description of each script:A short description of each script:
 + 0-sum_them_all.c: A function that returns the sum of all its parameters.
 + 1-print_numbers.c: A function that prints numbers, followed by a new line.
++ 2-print_strings.c: A function that prints strings, followed by a new line.
 +
