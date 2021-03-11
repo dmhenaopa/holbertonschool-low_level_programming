6a79b8f8903fbd325a71e66bc16553de33964c66
Diana Henao <2777@holbertonschool.com>
2021-03-11 19:47:28 +0000

    [ADD] 1-print_numbers.c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - C - Variadic functions
+* @separator: String to be printed between numbers
+* @n: Number of integers passed to the function
+*
+* Description: Function that prints numbers, followed by a new line.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	/*Reference to access*/
+	va_list list_to_print;
+	unsigned int i, value_to_print;
+
+	if (separator == NULL)
+		return;
+
+	/*Initialize list*/
+	va_start(list_to_print, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value_to_print = va_arg(list_to_print, int);
+		printf("%d", value_to_print);
+		printf("%s", separator);
+	}
+
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,4 @@
 A short description of each script:A short description of each script:A short description of each script:
 + 0-sum_them_all.c: A function that returns the sum of all its parameters.
++ 1-print_numbers.c: A function that prints numbers, followed by a new line.
 +
