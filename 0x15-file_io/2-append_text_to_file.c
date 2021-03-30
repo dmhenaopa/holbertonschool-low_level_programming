7db244f3581f9b8d8e519ac11c70f7936b3c89b1
Diana Henao <2777@holbertonschool.com>
2021-03-30 03:31:27 +0000

    :adhesive_bandage: Simple fix for a non-critical issue - when the text_content is NULL return 1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,6 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	/*Only to append the text_content*/
 	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (file_descriptor == -1)
