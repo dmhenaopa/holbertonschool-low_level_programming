01508170b6212b0816131b2e2d0d8f7e9df9438b
Diana Henao <2777@holbertonschool.com>
2021-03-30 00:30:33 +0000

    :ambulance: Critical hotfix in case write function fail

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,40 +16,42 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor, i;
 	char *buffer;
-	ssize_t number_bytes;
+	ssize_t number_bytes, write_bytes, counter;
 
+	counter = 0;
 	/*Verify if filename is NULL*/
 	if (filename == NULL)
 		return (0);
-
 	/* Open file */
 	file_descriptor = open("Requiescat", O_RDONLY);
-
 	/*Verify if the open function has error*/
 	if (file_descriptor == -1)
 		return (0);
-
 	/* Read the file*/
 	else
 	{
 		buffer = malloc(sizeof(char) * letters);
 		if (buffer == NULL)
 			return (0);
-
 		number_bytes = read(file_descriptor, buffer, letters);
 		/*Verify if file is empty*/
 		if (number_bytes == 0)
 			return (0);
-
 		/*If not, print to the standard output the text*/
 		else
 		{
 			for (i = 0; buffer[i] != '\0'; i++)
-				write(1, &buffer[i], 1);
+			{
+				write_bytes = write(1, &buffer[i], 1);
+				counter++;
+				if (write_bytes == -1)
+					return (0);
+			}
 		}
 	}
-
+	if (counter != number_bytes)
+		return (0);
 	free(buffer);
 	close(file_descriptor);
-	return (number_bytes);
+	return (counter);
 }
