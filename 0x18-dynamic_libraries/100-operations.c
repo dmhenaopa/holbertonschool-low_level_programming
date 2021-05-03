58b7c4c9c4307c5cd355a1eb3b92a5ac1c23bc0c
Diana Henao <2777@holbertonschool.com>
2021-05-03 22:20:59 +0000

    :hammer: Add development scripts 100-operations.c, dynamic library 100-operations.so, header file operations.h and upload README.md file

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include "operations.h"
+
+/**
+* add - addition operation
+* @a: number
+* @b: number
+*
+* Description: Operation to add a and b
+* Return: Result of adding a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - substraction operation
+* @a: number
+* @b: number
+*
+* Description: Operation to substract a to b
+* Return: Result of subtracting a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplication operation
+* @a: number
+* @b: number
+*
+* Description: Operation to multiplicate a and b
+* Return: Result of multiplication of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - division operation
+* @a: number
+* @b: number
+*
+* Description: Operation to divide a into b
+* Return: Result of divide a and b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - module operation
+* @a: number
+* @b: number
+*
+* Description: Operation to obtain the residue a / b
+* Return: Result of residue a / b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
