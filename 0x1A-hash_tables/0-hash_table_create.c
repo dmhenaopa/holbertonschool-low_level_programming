34a77aed2d94d1e854d89dc2b6fc5c255b522bc3
Diana Henao <2777@holbertonschool.com>
2021-05-27 20:02:18 +0000

    :hammer: Add development script 0-hash_table_create.c and header file hash_tables.h

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - C - Hash tables
+* @size: size of the array
+*
+* Description: Function that creates a hash table
+* Return: A pointer to the newly created hash table.
+* Otherwise, returns NULL.
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int i;
+
+	/* Allocate memory for hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Assign value to the hash table */
+	hash_table->size = size;
+
+	/* Allocate memory for the array of pointers */
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (hash_table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Initialize each pointer of array with NULL */
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+
+	return (hash_table);
+}
