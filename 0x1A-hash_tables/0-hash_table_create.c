b7fef5bd3aac7a6c4c5df9ea5c7866bacb84ab7d
Diana Henao <2777@holbertonschool.com>
2021-05-28 12:47:30 +0000

    :alembic: Perform experiment adding a condition to handle size 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	/* Allocate memory for hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
