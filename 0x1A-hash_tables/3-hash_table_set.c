9d12055e0709a942a577676afc47fc3ba68f0a25
Diana Henao <2777@holbertonschool.com>
2021-05-29 00:58:08 +0000

    :hammer: Update development script 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,29 +12,19 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int size_hash_t, key_value;
-	hash_node_t *node;
+	hash_node_t *node, *actual_node;
 
-	/* Verify the hash table and the key */
 	if (ht == NULL || strcmp(key, " ") == 0)
-	{
 		return (0);
-	}
-
-	/* Obtain the key for the hash table */
+	/* Obtain the index for the hash table */
 	size_hash_t = hash_table_size(ht);
 	key_value = (key_index((const unsigned char *) key, size_hash_t));
-
-	/* Allocate memory for the node */
+	/* Allocate memory for the node and put info in each node */
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-	{
 		return (0);
-	}
-
-	/* Put the information in each node */
 	node->key = (char *)key;
 	node->value = (char *)value;
-
 	/* Is the unique value in that index */
 	if (ht->array[key_value] == NULL)
 	{
@@ -44,10 +34,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* There is one or more nodes in that index */
 	else
 	{
-		node->next = ht->array[key_value];
-		ht->array[key_value] = node;
+		/* Traverse the linked list to found if the key exists */
+		while (actual_node != NULL && actual_node->next != NULL)
+		{
+			/* Compare if the key exists in the linked list */
+			if (strcmp(key, actual_node->key) == 0)
+				actual_node->value = (char *)value;
+			else
+				actual_node = actual_node->next;
+		}
+		/* If the key doesn't exist in the linked list */
+		if (strcmp(key, actual_node->key) != 0)
+		{
+			node->next = ht->array[key_value];
+			ht->array[key_value] = node;
+		}
 	}
-
 	return (1);
 }
 
