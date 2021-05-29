5767ab209d47e745949ca5f53809abcc850de830
Diana Henao <2777@holbertonschool.com>
2021-05-29 02:10:31 +0000

    :ambulance: Critical hotfix - add the condition if the pointer value is NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int key_value;
 	hash_node_t *node, *actual_node;
 
-	if (ht == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || value == NULL || strcmp(key, "") == 0)
 		return (0);
 	/* Obtain the index for the hash table */
 	key_value = (key_index((const unsigned char *) key, ht->size));
