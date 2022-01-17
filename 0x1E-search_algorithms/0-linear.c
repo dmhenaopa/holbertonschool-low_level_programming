c30e06c75b35bb6c3ca4d310432fca9962e7cc7f
Diana Henao <2777@holbertonschool.com>
2022-01-17 20:57:05 +0000

    :hammer: Add development script 0-linear.c, header file search_algos.h and main 0-main.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+* linear_search - C - Search Algorithms
+* @array: Pointer to first element of the array
+* @size: the number of elements in array
+* @value: the value to search for
+*
+* Description: A function that searches for a
+* value in an array of integers using the
+* Linear search algorithm
+* Return: The first index where value is located
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	int index = 0;
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (index);
+		}
+		printf("Value checked array[%d] = [%d]\n", index, array[i]);
+		index++;
+	}
+
+	return (-1);
+}
