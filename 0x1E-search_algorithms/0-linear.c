95f9344c28890319f6931abc45cd11999e01e400
Diana Henao <2777@holbertonschool.com>
2022-01-17 22:46:15 +0000

    :bug: Fix a bug - move function printf above the loop, so that it prints all attempts

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,11 +22,11 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", index, array[i]);
 		if (array[i] == value)
 		{
 			return (index);
 		}
-		printf("Value checked array[%d] = [%d]\n", index, array[i]);
 		index++;
 	}
 
