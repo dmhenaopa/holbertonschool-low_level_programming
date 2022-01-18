c6a6ba7f2eb07474b22ed07f4640045f5935fcec
Diana Henao <2777@holbertonschool.com>
2022-01-18 01:23:21 +0000

    :hammer: Update header file search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t lower, size_t higher);
 
 #endif /* _SEARCH_ALGOS_H_ */
